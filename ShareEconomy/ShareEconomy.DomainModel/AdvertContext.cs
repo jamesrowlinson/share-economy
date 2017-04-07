005a90cf159f1243e0f75500f46d128541f157b1
James Rowlinson <james_rowlinson@btinternet.com>
2017-04-07 20:53:10 +0100

    EF and ASP.NET MVC
    Added a ASP.NET MVC Project and a EF Project

---
+++ ShareEconomy/ShareEconomy.DomainModel/AdvertContext.cs
@@ -0,0 +1,11 @@
+﻿using ShareEconomy.Classes;
+using System.Data.Entity;
+
+
+namespace ShareEconomy.DomainModel
+{
+    public class AdvertContext : DbContext
+    {
+        public DbSet<Advert> Adverts { get; set; }
+    }
+}
